1973c5f0574d538541fac42bc1ab38537ecf6e18
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-09-03 21:32:17 -0300

    Update 2024-09-03   21h31
    Numeros
    Update 2024-09-03   21h31

---
+++ 01_numeros.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5.33
+print(a)
+print(b)
+print(a + b)  # soma
+print(a - b)  # subtracao
+print(a * b)  # multiplicacao
+print(a / b)  # divisao
+print(a % b)  # resto da divisao
+print(a // b)  # divisao considerando somente o valor inteiro, nao considera as casas decimais
+print(a ** b)  # potencia
